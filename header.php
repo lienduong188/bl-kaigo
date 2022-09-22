8ef4493a8468652570b0d04d85de52ff3b73e5c0
naka0000 <bestlife.nkmr@gmail.com>
2022-09-22 10:56:58 +0900

    faviconの追加

--- header.php
+++ header.php
@@ -16,6 +16,13 @@
 <?php wp_head(); ?>
 <meta name="keywords" content="訪問介護,老人介護,居宅,サービス,老人,福祉,認知,症,介護,生活,相談,支援,ケア,訪問,入浴,訪問,介護,と,は,事業,所,近く の, 施設,訪問介護員,ホームヘルパー" />
 <meta name="description" content="訪問介護のことなら<?= kPLATFORM_NAME ?>。住み慣れた環境で安心して過ごしたいという願いを叶えます。" />
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#f4cbc0">
 <!-- javascriptファイルへカスタムデータ属性を使って、イメージが格納されているディレクトリを渡すだけのscriptタグ -->
 <script id="data-bridge" data-themeuri="<?= get_template_directory_uri() ?>"></script>	
 <link rel="shortcut icon" href="<?= get_template_directory_uri() ?>/images/icon/favicon.ico">
